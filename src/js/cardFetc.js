eb0f1245941300225b50874b3bfc43623b74038b
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-11-18 00:37:22 +0200

    Добавляет рабочий pagination.
    Нужно сделать рефакторинг и , возможно, завернуть все функции в один класс (не точно, потому что долго)

--- src/js/cardFetc.js
+++ src/js/cardFetc.js
@@ -1,20 +1,20 @@
-import filmsCardTpl from '../templates/card-films.hbs';
+import filmsCardTpl from "../templates/card-films.hbs";
 
 const refs = {
-  cardContainet: document.querySelector('.js-card'),
+  cardContainet: document.querySelector(".js-card"),
 };
 
 fetch(
-  `https://api.themoviedb.org/3/trending/all/day?api_key=d91911ebb88751cf9e5c4b8fdf4412c9`,
+  `https://api.themoviedb.org/3/trending/all/day?api_key=d91911ebb88751cf9e5c4b8fdf4412c9`
 )
-  .then(response => response.json())
-  .then(data => {
-    renderFilmsCard(data.results.map(d => d));
+  .then((response) => response.json())
+  .then((data) => {
+    renderFilmsCard(data.results.map((d) => d));
   });
 
-renderFilmsCard(data.results.map(d => d));
+renderFilmsCard(data.results.map((d) => d));
 function renderFilmsCard(info) {
   const markup = filmsCardTpl(info);
-  refs.cardContainet.insertAdjacentHTML('beforeend', markup);
+  refs.cardContainet.insertAdjacentHTML("beforeend", markup);
   console.log(markup);
 }
