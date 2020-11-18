9718bb6a007801471ad0e555616d2055040ee11a
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-11-18 11:28:38 +0200

    добавляет стили командной модалки для всех брейкпоинтов

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import './js/modalFilmCard.js';
 import './js/modalOpen.js';
-// import './js/cardFetc.js';
+import './js/cardFetc.js';
 import './js/teamLightbox';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
--- src/js/teamLightbox.js
+++ src/js/teamLightbox.js
@@ -1,13 +1,11 @@
 import * as basicLightbox from 'basiclightbox';
-// import teamTpl from '../templates/team-card.hbs';
-// import teammates from '../templates/team.json';
-import ruslanaUrl from '../images/team-ruslana.jpg';
+import ruslanaUrl from '../images/team-ruslana.jpeg';
 import olgaUrl from '../images/team-olga.jpg';
-import dariaUrl from '../images/team-daria.jpg';
-import denisUrl from '../images/team-denis.jpg';
+import dariaUrl from '../images/team-daria.jpeg';
+import denisUrl from '../images/team-denis.jpeg';
 import ihorUrl from '../images/team-ihor.jpg';
 import katyaUrl from '../images/team-katya.jpg';
-import vladymyrUrl from '../images/team-vladymyr.jpg';
+import vladymyrUrl from '../images/team-vladymyr.jpeg';
 import aleksandrUrl from '../images/team-aleksandr.jpg';
 import spriteUrl from '../images/sprite.svg';
 
@@ -15,7 +13,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${ruslanaUrl}" alt="Ruslana" class="team-image">
     <p class="team-name">Ruslana</p>
     <p class="team-role">Team Leader</p>
-    <a href="https://github.com/RuslanaLogosha" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/RuslanaLogosha" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -23,7 +21,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${olgaUrl}" alt="Olga" class="team-image">
     <p class="team-name">Olga</p>
     <p class="team-role">Scrum Master</p>
-    <a href="https://github.com/levshukova" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/levshukova" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -31,7 +29,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${dariaUrl}" alt="Daria" class="team-image">
     <p class="team-name">Daria</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/Daria-Churkina" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Daria-Churkina" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -39,7 +37,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${denisUrl}" alt="Denis" class="team-image">
     <p class="team-name">Denis</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -47,7 +45,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${ihorUrl}" alt="Ihor" class="team-image">
     <p class="team-name">Ihor</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/taraiihor" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/taraiihor" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -55,7 +53,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${katyaUrl}" alt="Katya" class="team-image">
     <p class="team-name">Katya</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/Kateryna-Urbanovych" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Kateryna-Urbanovych" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -63,7 +61,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${vladymyrUrl}" alt="Vladymyr" class="team-image">
     <p class="team-name">Vladymyr</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/created-with-love" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/created-with-love" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
@@ -71,7 +69,7 @@ const markup = `<div class="team-wrapper"><div class="team-card">
     <img src="${aleksandrUrl}" alt="Aleksandr" class="team-image">
     <p class="team-name">Aleksandr</p>
     <p class="team-role">Developer</p>
-    <a href="https://github.com/AleksMkm" target="_blank"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/AleksMkm" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div></div>`;
