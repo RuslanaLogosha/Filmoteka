e14203de656916f52786e7dcd6d0c3cbd60a7226
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-11-19 14:31:24 +0200

    правит датасет в hbs для работы модалки

--- src/js/modalFilmCard.js
+++ src/js/modalFilmCard.js
@@ -1,52 +1,42 @@
-import modalFilmCard from "../templates/modalFilmCard.hbs";
+import modalFilmCard from '../templates/modalFilmCard.hbs';
 
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-
-let page = 1;
+// let page = 1;
 const apiKey = 'd91911ebb88751cf9e5c4b8fdf4412c9';
 
 const cardFilm = document.querySelector('.card__colection');
 cardFilm.addEventListener('click', openModal);
 
-
 function fetchOneMovieInfo(movie_id) {
   const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${apiKey}`;
-  return fetch(url)
-    .then(response => response.json())        
+  return fetch(url).then(response => response.json());
 }
 
 function openModal(e) {
   e.preventDefault();
 
-  fetchOneMovieInfo(e.target.dataset.id).then(data => {
+  fetchOneMovieInfo(e.target.dataset.id)
+    .then(data => {
+      if (e.target.nodeName !== 'IMG') return;
 
-    if (e.target.nodeName !== 'IMG') return;
-    
-  const markup = modalFilmCard(data);
-    const modal = basicLightbox.create(markup);
+      const markup = modalFilmCard(data);
+      const modal = basicLightbox.create(markup);
 
-    modal.show();
+      modal.show();
 
-  window.addEventListener('keydown', closeModalHandler);
+      window.addEventListener('keydown', closeModalHandler);
 
-function closeModalHandler(e) {
-  if (e.code === 'Escape') {
-    modal.close();
-    window.removeEventListener('keydown', closeModalHandler);
-  }
+      function closeModalHandler(e) {
+        if (e.code === 'Escape') {
+          modal.close();
+          window.removeEventListener('keydown', closeModalHandler);
+        }
+      }
+    })
+    .then(data => {})
+    .catch(error => {
+      console.log('oops!');
+    });
 }
-    
-  }).then(data => {}).catch(error => {console.log("oops!")})
-};
-
-
-
-
-
-
-
-
-
-
