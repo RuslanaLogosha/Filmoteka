3a980ae0f337fd0334d4cbe0099e890f7b5d03ce
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-11-21 23:09:21 +0200

    Updates

--- library.cbab63f8.js
+++ library.cbab63f8.js
@@ -4334,7 +4334,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51593" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51620" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
